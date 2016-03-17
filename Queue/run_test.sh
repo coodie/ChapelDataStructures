ba4b78132387c6cfdd740eb439cfdb5dfb9e1a9d
coodie <poppinggoovie@gmail.com>
2016-03-18 00:15:50 +0100

    Fixed run_test.sh

--- Queue/run_test.sh
+++ Queue/run_test.sh
@@ -1,2 +1,2 @@
 [ -z "$CHPL_HOME" ] && echo "Need to set $CHPL_HOME" && exit 1;
-$CHPL_HOME/util/start_test -compopts -M -compopts $PWD/ -compopts --print-search-dirs ./test 
+$CHPL_HOME/util/start_test ./test 
